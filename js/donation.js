c0ac6a023f2a705ab0fa4f7ca2b1ca4b8f21bef5
qazimustafijur <qazimustafijur@gmail.com>
2024-09-25 00:12:59 +0600

    button color working fine

--- js/donation.js
+++ js/donation.js
@@ -1,1 +1,5 @@
 addEventListenerById("click",'noakhaliDonate',increaseBalance);
+addEventListenerById("click",'feniDonate',increaseBalance);
+addEventListenerById("click",'protestDonate',increaseBalance);
+addEventListenerById("click",'donationBtn',donationButtonColor);
+addEventListenerById("click",'historyBtn',donationButtonColor);
--- js/shared.js
+++ js/shared.js
@@ -1,10 +1,14 @@
+//increase the balance of the donation section from where the function called
+//and call function to decrease amount from main balance.
 function increaseBalance(e){
     e.preventDefault();
     let id = e.target.parentElement.id;
-    let input = document.getElementById(id+"Input").value;
+    let inputElement = document.getElementById(id+"Input");
+    let input = inputElement.value;
+    inputElement.value = "";
     // validating input 
     if (isNaN(input) || input<1){
-        alert('Invaild input!');
+        alert('Invalid input!');
         return 0;
     }
     input=parseFloat(input);
@@ -12,12 +16,14 @@ function increaseBalance(e){
     let target = document.getElementById(id+"Balance");
     let output = parseFloat(target.innerText)+input;
     if(isNaN(output)){
-        alert('Invaild input!');
+        alert('Invalid input!');
         return 0;
     }
     target.innerText = output;
     decreaseMainBalance(input);
 }
+
+//decreases the main balance. this function is called from inside the increaseBalance function
 function decreaseMainBalance(decrease){
     let balanceElement = document.getElementById('mainBalance');
     if(isNaN(balanceElement.innerText)){
@@ -27,11 +33,20 @@ function decreaseMainBalance(decrease){
     balanceElement.innerText= parseFloat(balanceElement.innerText)-decrease;
 
 }
-function getInnerText(id){
-    document.getElementById(id);
-
-}
+// eventlistener adding by id and function
 function addEventListenerById(event,id,func){
     
     document.getElementById(id).addEventListener(event,func);
+}
+function donationButtonColor(e){
+    e.preventDefault();
+    let a = document.getElementById('donationSec');
+    let b = document.getElementById('historySec');
+    let c = document.getElementById('donationBtn') 
+    let d = document.getElementById('historyBtn');
+    e.target.id=="donationBtn"?[a,b]=[b,a]:[c,d]=[d,c];
+    a.classList.add('hidden');
+    b.classList.remove('hidden');
+    c.classList.add('bg-[#B4F461]');
+    d.classList.remove('bg-[#B4F461]');
 }
